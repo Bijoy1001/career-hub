0649dcc640874f46c42ba3637f3b1914616ec887
Bijoy1001 <bijoykhan1001@gmail.com>
2023-10-22 01:50:11 +0600

    3rd commit

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,11 +1,23 @@
+import { createFactory, useEffect, useState } from "react";
 
 const CategoryList = () => {
+    const [categori, setCategori] = useState ([]);
+    const [logo, category_name, availability] = categori;
+
+    useEffect(() => {
+            fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategori(data));
+    }, [])
     return (
-        <div className="text-center">
+        <div>
+            <div className="text-center mt-8 mb-8">
             <h2 className="font-bold text-4xl">Job Category List</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+           
+               
+            </div>
         </div>
-    );
-};
+    );};
 
 export default CategoryList;
