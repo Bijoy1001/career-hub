8d219c60d7542f8bddd8fc1f3a1ae6103ac211c5
Bijoy1001 <bijoykhan1001@gmail.com>
2023-10-21 23:50:09 +0600

    banner design

--- src/components/BannerDesign/BannerDesign.jsx
+++ src/components/BannerDesign/BannerDesign.jsx
@@ -1,7 +1,20 @@
 const BannerDesign = () => {
     return (
-        <div className="text-center">
-            <h2 className="font-bold text-6xl">One Step Closer To Your Dream Job</h2>
+        <div className="bg-blue-950">
+            <div className="flex gap-6 pl-4">
+                <div>
+                    <h1 className="font-bold text-6xl text-white mt-10">One Step <br />
+                         Closer To Your <br />
+                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] "> Dream Job</span></h1>
+                         <p className="mt-8 mb-8">Explore thousands of job opportunities with all the <br />
+                          information you need. Its your future. Come find it. Manage all <br />
+                           your job application from start to finish.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                </div>
+                <div>
+                    <img src="/src/assets/images/user.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
